b2b52f19870a57f3fbe6fa70a54685527cc377f9
mike <mike@lcd-outlet.com>
2020-02-10 20:42:30 +0000

    repl -> Dotfile

---
+++ repl/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern int yylex(); 
+int main(){
+  yylex();
+  return 0;
+}
---
+++ repl/utils.h
@@ -0,0 +1,12 @@
+void dot_file(char* file_name,char *r_w_a,char *text){
+  FILE *f = fopen(file_name, r_w_a);
+  if (f == NULL)
+  {
+      printf("Error opening file!\n");
+      exit(1);
+  }
+  fprintf(f,"%s","graph {\n");
+  fprintf(f, "%s\n", text);
+  fprintf(f,"%s","}\n");
+  fclose(f);
+}
